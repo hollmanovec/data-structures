1e7653383ffc81ca951c79f744c5064832bf6a11
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 20:38:31 +0200

    Topping

---
+++ pythonProject1/pizza app/objects/topping.py
@@ -0,0 +1,15 @@
+class Topping:
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+    def __str__(self):
+        return f"{self.__name}, cena {self.__price}Kč za jednotku"
+
+    def get_name(self):
+        return self.__name
+
+    def get_price(self):
+        return self.__price
+
+    # přidat možnost zdražení
