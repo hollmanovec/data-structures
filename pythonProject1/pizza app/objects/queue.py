8bb55fb97a02483be7850ed8f92762263efb206b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 21:03:13 +0200

    Queue

---
+++ pythonProject1/pizza app/objects/queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.queue = []
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def is_full(self):
+        return len(self.queue) == self.max_size
+
+    def add_new(self, new_item):
+        if not self.is_full():
+            self.queue.append(new_item)
+            return True
+        return f"Kapacita naplněna, zkustě to prosím později."
+
+    def read_remove(self):
+        if not self.is_empty():
+            item = self.queue[0]
+            self.queue.pop(0)
+            return item
